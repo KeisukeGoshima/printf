047589c13bcadef97e35a21be5d7d025419611ae
goshima <goshima.k.ab@gmail.com>
2022-10-11 23:23:50 +0900

    v4

--- ft_printf.h
+++ ft_printf.h
@@ -22,7 +22,7 @@ char	*ft_strdup(const char *s);
 size_t	ft_strlen(const char *s);
 int		ft_toupper(int c);
 void	format_distribute(char *str, va_list ap, int *flag);
-void	ft_putstr_fd(char *s, int fd);
-void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd, int *flag);
+void	ft_putchar_fd(char c, int fd, int *flag);
 
 #endif
